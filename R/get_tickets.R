d14f65f7c3f53c7c9ad0559824ae0224de73658d
Chris Umphlett <christopher.umphlett@gmail.com>
2023-05-02 11:40:02 +0000

    version 0.3.0

--- R/get_tickets.R
+++ R/get_tickets.R
@@ -40,8 +40,6 @@
 #' @param subdomain Your organization's Zendesk sub-domain.
 #' @param start_time String with a date or datetime to get all
 #' tickets modified after that date.
-#' @param end_time String with a date or datetime to get all
-#' tickets modified before that date.
 #' @param remove_cols Vector of column names to remove from the results.
 #'
 #' @return a Data Frame containing all tickets after the
@@ -62,12 +60,11 @@
 #'   start_time = "2021-01-31 00:00:00", end_time = "2021-01-31 23:59:59"
 #' )
 #' }
-get_tickets <- function(email_id, token, subdomain, start_time, end_time,
+get_tickets <- function(email_id, token, subdomain, start_time,
                         remove_cols = NULL) {
   user <- paste0(email_id, "/token")
   pwd <- token
   unix_start <- to_unixtime(as.POSIXct(start_time))
-  unix_end <- to_unixtime(as.POSIXct(end_time))
 
   request_ticket <- list()
   stop_paging <- FALSE
@@ -81,25 +78,23 @@ get_tickets <- function(email_id, token, subdomain, start_time, end_time,
     )
 
     request_ticket[[i]] <- httr::RETRY("GET",
-      url = url,
-      httr::authenticate(user, pwd),
-      times = 4,
-      pause_min = 10,
-      terminate_on = NULL,
-      terminate_on_success = TRUE,
-      pause_cap = 5
+                                       url = url,
+                                       httr::authenticate(user, pwd),
+                                       times = 4,
+                                       pause_min = 10,
+                                       terminate_on = NULL,
+                                       terminate_on_success = TRUE,
+                                       pause_cap = 5
     )
-    message(paste0(i, " - a"))
     unix_start <- (jsonlite::fromJSON(httr::content(
       request_ticket[[i]],
       "text"
     ), flatten = TRUE))$end_time
     if ((jsonlite::fromJSON(httr::content(request_ticket[[i]], "text"),
-      flatten = TRUE
-    ))$end_time >= unix_end) {
+                            flatten = TRUE
+    ))$end_of_stream == TRUE) {
       stop_paging <- TRUE
     }
-    message(paste0(i, " - b"))
     i <- i + 1
   }
 
@@ -112,6 +107,7 @@ get_tickets <- function(email_id, token, subdomain, start_time, end_time,
 
   pivot_data_frame <- function(c) {
     pivot_df <- as.data.frame(tickets$custom_fields[c]) %>%
+      mutate(across(.cols = .data$value, as.character)) %>%
       tidyr::pivot_wider(names_from = .data$id, values_from = .data$value) %>%
       select(-remove_cols)
   }
